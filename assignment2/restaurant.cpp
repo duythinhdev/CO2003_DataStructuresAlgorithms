3d3a2dbb4a8b4014750bd4b6800d742885b5fd13
MSI\ADMIN <do.duythinh1@gmail.com>
2023-04-25 22:12:40 +0700

    change simulate

--- assignment2/restaurant.cpp
+++ assignment2/restaurant.cpp
@@ -1,6 +1,37 @@
 #include "main.h"
+#define ACTION_REG "REG"
 
 void simulate(string filename)
 {
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
+            int end = line.find(STR_NULL);
+            string commandName = string(line.substr(0, end));
+            clearCommandData(&line);
+            if (commandName == ACTION_REG) {
+                reg(line);
+            }
+        }
+        myfile.close();
+    }
 	return;
+}
+
+
+
+int countSpace(string command, string del) {
+    int end = command.find(del);
+    int count = 0;
+    while (end != -1) {
+        count++;
+        command.erase(command.begin(), command.begin() + end + 1);
+        end = command.find(del);
+    }
+
+    return count;
+}
+
+void clearCommandData(string *command) {
+    int end = command->find(" ");
+    command->erase(command->begin(), command->begin() + end + 1);
 }
